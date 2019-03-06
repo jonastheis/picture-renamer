aebcac645bd48a263e3fb7abdfa638fd4c037905
jonastheis <work@jonastheis.de>
2019-03-06 16:34:15 +0100

    Add simple Qt GUI

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # picture-renamer
+
+
+## Setup
+```bash
+python3 -m venv venv 
+source venv/bin/activate
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,43 +1,132 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from os import listdir, rename
-from os.path import isfile, join
-import PIL.Image
-
-MY_PATH = "/Users/jonastheis/Desktop/Cambodia"
-
-# find all wrong named images
-#files = filter(lambda x: "iOS" in x and x.endswith(".jpg"), [f for f in listdir(MY_PATH) if isfile(join(MY_PATH, f))])
-files = filter(lambda x: x.endswith(".jpg") or x.endswith(".JPG"), [f for f in listdir(MY_PATH) if isfile(join(MY_PATH, f))])
+import sys
+from PySide2.QtWidgets import *
+from PySide2.QtCore import Qt
 
 
-print files
+class MainWindowWidget(QWidget):
+    def __init__(self):
+        super(MainWindowWidget, self).__init__()
 
-for n in files:
-    try:
+        self.drop_zone = DropZoneWidget()
+        self.settings = SettingsWidget()
 
-        img = PIL.Image.open(join(MY_PATH, n))
-        date_total = img._getexif()[306]
-
-        date_string, time_string = date_total.split()
-
-        time_string = time_string.replace(":", ".")
-        date_string = date_string.replace(":", "-")
-
-        # optional: add hours to time
-        if False:
-            to_add = 9  # change to time shift
-            hour = int(time_string.split(".")[0])
-            hour += to_add
-
-            time_string = "" + str(hour) + time_string[2:]
+        # A Vertical layout to include the button layout and then the image
+        layout = QVBoxLayout()
+        layout.addWidget(self.drop_zone)
+        layout.addWidget(QHLine())
+        layout.addWidget(self.settings)
 
 
-        date_total = date_string + " " + time_string + ".jpg"
+        self.setLayout(layout)
+        self.setFixedSize(500, 500)
+        self.show()
+        self.setFocus()
 
-        rename(join(MY_PATH, n), join(MY_PATH, date_total))
-        print date_total
 
-    except KeyError:
-        pass
+class QHLine(QFrame):
+    def __init__(self):
+        super(QHLine, self).__init__()
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
+
+
+class SettingsWidget(QWidget):
+    def __init__(self):
+        super(SettingsWidget, self).__init__()
+
+        time_shift_layout, self.time_shift = self.init_time_shift()
+
+        settings_label = QLabel('<font size=5>Settings</font>')
+
+        layout = QVBoxLayout()
+        layout.addWidget(settings_label)
+        layout.addLayout(time_shift_layout)
+
+
+        self.setLayout(layout)
+
+        self.show()
+
+    def init_time_shift(self):
+        time_shift_layout = QHBoxLayout()
+
+        description_label = QLabel('Time shift in hours:')
+        time_shift_layout.addWidget(description_label)
+
+        time_shift = QSpinBox()
+        time_shift.setRange(-23, 23)
+        time_shift.setValue(0)
+
+        time_shift_layout.addWidget(time_shift)
+
+        return time_shift_layout, time_shift
+
+    def get_time_shift(self):
+        shift = self.time_shift.value()
+        return shift
+
+
+class DropZoneWidget(QWidget):
+    def __init__(self):
+        super(DropZoneWidget, self).__init__()
+        self.drop_label = QLabel('<font size=40>Drop parent folder</font>')
+        self.drop_label.setAlignment(Qt.AlignCenter)
+        self.or_label = QLabel('or')
+        self.or_label.setAlignment(Qt.AlignCenter)
+        self.select_button = QPushButton('Select parent folder')
+        self.select_button.clicked.connect(self.select_button_click)
+
+        # A Vertical layout to include the button layout and then the image
+        layout = QVBoxLayout()
+        layout.addStretch()
+        layout.addWidget(self.drop_label)
+        layout.addWidget(self.or_label)
+        layout.addWidget(self.select_button)
+        layout.addStretch()
+
+
+        self.setLayout(layout)
+
+        # Enable dragging and dropping onto the GUI
+        self.setAcceptDrops(True)
+
+        self.show()
+
+    # The following three methods set up dragging and dropping for the app
+    def dragEnterEvent(self, e):
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+
+    def dragMoveEvent(self, e):
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+
+    def dropEvent(self, e):
+        if e.mimeData().hasUrls:
+            e.setDropAction(Qt.CopyAction)
+            e.accept()
+
+            # only get 1 element from drop
+            path = e.mimeData().urls()[0].path()
+            self.set_folder(path)
+        else:
+            e.ignore()
+
+    def select_button_click(self):
+        path = QFileDialog.getExistingDirectory(self, 'Select folder')
+        self.set_folder(path)
+
+    def set_folder(self, folder_name):
+        # TODO: call start rename from MainWindowWidget
+        print(folder_name)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    main_widget = MainWindowWidget()
+    sys.exit(app.exec_())
